984cb7f76b368615f9bcab074f6cd8ca4bc974f9
jobi <jorn.ola.birkeland@nav.no>
2023-05-22 15:51:14 +0200

    Utvider Fagsystem med barnetrygd og enslig forelder for å kunne få til tester

--- src/main/kotlin/no/nav/dagpenger/kontrakter/utbetaling/Fagsystem.kt
+++ src/main/kotlin/no/nav/dagpenger/kontrakter/utbetaling/Fagsystem.kt
@@ -1,5 +1,7 @@
 package no.nav.dagpenger.kontrakter.utbetaling
 
 enum class Fagsystem(val kode: String) {
-    Dagpenger("DP")
+    Dagpenger("DP"),
+    Barnetrygd("BATR"),
+    EnsligForelder("EFOG")
 }
