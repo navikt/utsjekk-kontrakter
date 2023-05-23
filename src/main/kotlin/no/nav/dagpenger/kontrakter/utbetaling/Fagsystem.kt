a617e0d862d52e1676f0ec942eb4fcfe6318f893
jobi <jorn.ola.birkeland@nav.no>
2023-05-23 09:49:44 +0200

    Fjerner BA og EF fra Fagsystem

--- src/main/kotlin/no/nav/dagpenger/kontrakter/utbetaling/Fagsystem.kt
+++ src/main/kotlin/no/nav/dagpenger/kontrakter/utbetaling/Fagsystem.kt
@@ -1,7 +1,5 @@
 package no.nav.dagpenger.kontrakter.utbetaling
 
 enum class Fagsystem(val kode: String) {
-    Dagpenger("DP"),
-    Barnetrygd("BATR"),
-    EnsligForelder("EFOG")
+    Dagpenger("DP")
 }
