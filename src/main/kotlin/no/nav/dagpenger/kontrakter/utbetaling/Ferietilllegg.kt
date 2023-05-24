61f700bf8190eae00dc2d6b26f4832c45dfec8a3
jobi <jorn.ola.birkeland@nav.no>
2023-05-24 17:25:49 +0200

    Legger til enum for Ferietillegg og tar det i bruk for å generere klassifisering fra StønadType

---
+++ src/main/kotlin/no/nav/dagpenger/kontrakter/utbetaling/Ferietilllegg.kt
@@ -0,0 +1,6 @@
+package no.nav.dagpenger.kontrakter.utbetaling
+
+enum class Ferietilllegg {
+    VANLIG,
+    AVDØD
+}
--- src/main/kotlin/no/nav/dagpenger/kontrakter/utbetaling/StønadType.kt
+++ src/main/kotlin/no/nav/dagpenger/kontrakter/utbetaling/StønadType.kt
@@ -7,11 +7,27 @@ enum class StønadType {
     DAGPENGER_EOS,
 }
 
-fun StønadType.tilKlassifisering() = when (this) {
-    StønadType.DAGPENGER_ARBEIDSSOKER_ORDINAER -> "DPORAS"
-    StønadType.DAGPENGER_PERMITTERING_ORDINAER -> "DPPEAS"
-    StønadType.DAGPENGER_PERMITTERING_FISKEINDUSTRI -> "DPPEFI"
-    StønadType.DAGPENGER_EOS -> "DPDPASISP1"
+fun StønadType.tilKlassifisering(ferietilllegg: Ferietilllegg? = null) = when (this) {
+    StønadType.DAGPENGER_ARBEIDSSOKER_ORDINAER -> when(ferietilllegg) {
+        Ferietilllegg.VANLIG -> "DPORASFE"
+        Ferietilllegg.AVDØD -> "DPORASFE-IOP"
+        null -> "DPORAS"
+    }
+    StønadType.DAGPENGER_PERMITTERING_ORDINAER -> when(ferietilllegg) {
+        Ferietilllegg.VANLIG -> "DPPEASFE1"
+        Ferietilllegg.AVDØD-> "DPPEASFE1-IOP"
+        null -> "DPPEAS"
+    }
+    StønadType.DAGPENGER_PERMITTERING_FISKEINDUSTRI -> when(ferietilllegg) {
+        Ferietilllegg.VANLIG -> "DPPEFIFE1"
+        Ferietilllegg.AVDØD -> "DPPEFIFE1-IOP"
+        null-> "DPPEFI"
+    }
+    StønadType.DAGPENGER_EOS -> when(ferietilllegg) {
+        Ferietilllegg.VANLIG -> "DPFEASISP"
+        Ferietilllegg.AVDØD -> throw IllegalArgumentException("Eksport-gruppen har ingen egen kode for ferietillegg til avdød")
+        null -> "DPDPASISP1"
+    }
 }
 
 fun StønadType.tilFagsystem() = when (this) {
