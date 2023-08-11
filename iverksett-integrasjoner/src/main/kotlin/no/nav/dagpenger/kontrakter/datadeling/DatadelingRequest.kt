2f3c91d39590af220d0a272470e320783a8404f9
Igor Shuliakov <igor.shuliakov@nav.no>
2023-08-11 10:14:48 +0200

    DatadelingRequest

---
+++ iverksett-integrasjoner/src/main/kotlin/no/nav/dagpenger/kontrakter/datadeling/DatadelingRequest.kt
@@ -0,0 +1,9 @@
+package no.nav.dagpenger.kontrakter.datadeling
+
+import java.time.LocalDate
+
+data class DatadelingRequest(
+    val personIdent: String,
+    val fraOgMedDato: LocalDate,
+    val tilOgMedDato: LocalDate?
+)
