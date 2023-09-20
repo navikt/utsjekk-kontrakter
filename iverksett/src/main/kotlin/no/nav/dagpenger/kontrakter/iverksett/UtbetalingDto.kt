a5c5138d71ecba925d875d5ac2dc75093bfb659c
Stephen Ramthun <stephenramthun@gmail.com>
2023-09-20 12:59:24 +0200

    Spesifiser at stønadstype må være enten StønadTypeDagpenger eller StønadTypeTiltakspenger
    Co-authored-by: Martine Enger <marenger@student.matnat.uio.no>

--- iverksett/src/main/kotlin/no/nav/dagpenger/kontrakter/iverksett/UtbetalingDto.kt
+++ iverksett/src/main/kotlin/no/nav/dagpenger/kontrakter/iverksett/UtbetalingDto.kt
@@ -4,13 +4,18 @@ import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.dagpenger.kontrakter.felles.StønadType
 import java.time.LocalDate
 import no.nav.dagpenger.kontrakter.felles.StønadTypeDagpenger
+import no.nav.dagpenger.kontrakter.felles.StønadTypeTiltakspenger
 
 data class UtbetalingDto(
 
     val belopPerDag: Int,
     val fraOgMedDato: LocalDate,
     val tilOgMedDato: LocalDate,
-    @Schema(required = false, defaultValue = "DAGPENGER_ARBEIDSSOKER_ORDINAER")
+    @Schema(
+        required = false,
+        oneOf = [StønadTypeDagpenger::class, StønadTypeTiltakspenger::class],
+        defaultValue = "DAGPENGER_ARBEIDSSOKER_ORDINAER"
+    )
     val stonadstype: StønadType = StønadTypeDagpenger.DAGPENGER_ARBEIDSSOKER_ORDINAER,
     @Schema(required = false, description = "Settes kun ved ferietillegg")
     val ferietillegg: Ferietillegg? = null
