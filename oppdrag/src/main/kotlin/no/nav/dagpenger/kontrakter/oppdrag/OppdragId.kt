f5e6cbe74e574a485126fcd328e7e501c380f5a1
Martine Enger <marenger@student.matnat.uio.no>
2024-01-18 10:02:59 +0100

    Bruk GeneriskId som type for behandlingId i OppdragId-klassen

--- oppdrag/src/main/kotlin/no/nav/dagpenger/kontrakter/oppdrag/OppdragId.kt
+++ oppdrag/src/main/kotlin/no/nav/dagpenger/kontrakter/oppdrag/OppdragId.kt
@@ -1,12 +1,13 @@
 package no.nav.dagpenger.kontrakter.oppdrag
 
 import no.nav.dagpenger.kontrakter.felles.Fagsystem
-import java.util.UUID
+import no.nav.dagpenger.kontrakter.felles.GeneriskId
 
+@Suppress("unused")
 data class OppdragId(
     val fagsystem: Fagsystem,
     val personIdent: String,
-    val behandlingsId: UUID
+    val behandlingId: GeneriskId,
 ) {
-    override fun toString(): String = "OppdragId(fagsystem=$fagsystem, behandlingsId=$behandlingsId)"
+    override fun toString(): String = "OppdragId(fagsystem=$fagsystem, behandlingsId=$behandlingId)"
 }
